78f8808bec2c2fb836b0bb2bb11e3718080ff49b
Parsa Hejabi <parsa.hejabi@gmail.com>
2019-01-27 14:06:11 +0330

    Added User Model (Not complete!).

---
+++ Instagram/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Instagram.Models
+{
+    public class User
+    {
+        [StringLength(30)]
+        [Required]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [StringLength(30, MinimumLength = 8)]
+        [Required]
+        public string Password { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string FamilyName { get; set; }
+
+    }
+}
