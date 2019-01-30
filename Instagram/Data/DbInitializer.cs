043161fb647796ea9cd6abd8d9578d89b667071c
Parsa Hejabi <parsa.hejabi@gmail.com>
2019-01-30 21:58:06 +0330

    Updated DbInitializer and Create Time for Comments and Posts works!

--- Instagram/Data/DbInitializer.cs
+++ Instagram/Data/DbInitializer.cs
@@ -29,6 +29,33 @@ namespace Instagram.Data
                 context.Users.Add(user);
             }
             context.SaveChanges();
+
+            var posts = new Post[]
+            {
+                new Post {UserID=1,Caption="Initial Post For Admin",ImagePath="./1.jpg"},
+                new Post {UserID=1,Caption="Second Post For Admin",ImagePath="./2.jpg"},
+                new Post {UserID=2,Caption="ParsaFirst",ImagePath="./3.jpg"},
+                new Post {UserID=3,Caption="NikiFirst",ImagePath="./4.jpg"}
+            };
+
+            foreach (Post post in posts)
+            {
+                context.Posts.Add(post);
+            }
+            context.SaveChanges();
+
+            var comments = new Comment[]
+            {
+                new Comment {PostID=1,UserID=1,Content="Im Admin Commenting on my post."},
+                new Comment {PostID=2,UserID=1,Content="Im Admin Commenting on Parsa post."},
+                new Comment {PostID=3,UserID=2,Content="Parsa on Niki post."}
+            };
+
+            foreach (Comment comment in comments)
+            {
+                context.Comments.Add(comment);
+            }
+            context.SaveChanges();
         }
     }
 }
