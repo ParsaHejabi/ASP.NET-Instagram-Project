2d486fb86ad3e03b07fddef8f61586cd09a137f0
Parsa Hejabi <parsa.hejabi@gmail.com>
2019-02-04 02:50:41 +0330

    Enforced HTTPS (might have bugs!).

--- Instagram/Startup.cs
+++ Instagram/Startup.cs
@@ -49,8 +49,11 @@ namespace Instagram
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
